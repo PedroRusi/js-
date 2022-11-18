2c1543b22a6c6f5d80c74dd45ba95272d196e2bf
Vladimir_Yamukov <114881313+PedroRusi@users.noreply.github.com>
2022-11-18 15:28:42 +0300

    Add files via upload

--- Js Подготовка/assets/js/script.js
+++ Js Подготовка/assets/js/script.js
@@ -60,6 +60,7 @@ function insert_Row() {
 
 
 
+
 function changeContent() {
     let table = document.getElementById('myTable');
     let tbody = table.children[0]
@@ -108,3 +109,60 @@ function getOptions() {
 }
 
 
+
+function Calculate(event) {
+    event.preventDefault();
+    let result = document.createElement('p')
+    result.classList.add('result')
+    let form = document.forms[5]
+    let sum = form.elements.select.value
+    sum = Math.round(sum * (1 + 0.01 * form.elements.complexity.value))
+    sum = sum * form.elements.range.value
+    let radio = form.elements.radio
+    for (let rad of radio) {
+        if (rad.checked) {
+            sum = sum * rad.value
+        }
+    }
+    if (form.elements.check.checked) {
+        sum = Math.round(sum * 0.85)
+    } else {
+        sum = Math.round(sum)
+        
+    }
+    result.innerText = "Услуга будет стоить: " + sum
+    if (form.lastElementChild.className != 'result') {
+         form.appendChild(result)  
+    } else {
+        form.lastElementChild.innerHTML = result.innerHTML
+    }
+}
+
+
+
+
+function addTable(event) {
+    event.preventDefault();
+    let form = document.forms.namedItem('table')
+    let name = form.elements.firstname.value
+    let lastname = form.elements.lastname.value
+    let blockTable = document.getElementById('blockTable')
+    let elem = document.createElement('div')
+    elem.className = 'block__name'
+    elem.classList.add('redact')
+    elem.innerHTML = `<a class="first__name first">${name}</a><a class="last__name last">${lastname}</a>`
+    blockTable.appendChild(elem)
+    console.log(elem);
+}
+
+
+let BlockTable = document.getElementById('blockTable')
+BlockTable.addEventListener('click', function(elem) {
+    let target = elem.target.closest('.redact.first', 'redact.last')
+    let firstname = target.firstElementChild.innerText
+    let lastname = target.lastElementChild.innerText
+    let area = document.createElement('textarea')
+    area.value = firstname + lastname
+    target.innerHTML = ''
+    target.appendChild(area)
+})
